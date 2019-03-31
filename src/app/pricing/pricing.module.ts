345b6ec28af4677b071a9f9f38dcd74565dbefa7
Colton Morales <developmentbycolton@outlook.com>
2019-03-30 19:58:00 -0500

    Added pricing info

--- src/app/pricing/pricing.module.ts
+++ src/app/pricing/pricing.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { PricingComponent } from './pricing/pricing.component';
 import { PricingOverviewComponent } from './pricing-overview/pricing-overview.component';
 import { CoreModule } from '../core/core.module';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { PricingDetailsComponent } from './pricing-details/pricing-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     PricingComponent,
-    PricingOverviewComponent
+    PricingOverviewComponent,
+    DisclaimerComponent,
+    PricingDetailsComponent
   ],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    RouterModule
   ],
   entryComponents: [
     PricingComponent
--- src/app/pricing/pricing/pricing.component.ts
+++ src/app/pricing/pricing/pricing.component.ts
@@ -11,5 +11,4 @@ export class PricingComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
